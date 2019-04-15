4172df345264ba195b4744a9bcf1a2029fd21a7d
aj7774 <aj7774@gmail.com>
2019-04-14 20:22:06 -0400

    more screens
    [ADD] agilanApp screen

--- app/src/navigation/AppScreens.js
+++ app/src/navigation/AppScreens.js
@@ -1,11 +1,18 @@
 import { Navigation } from "react-native-navigation";
 
 import Welcome from "../components/Welcome/Welcome";
+import AgilanApp from "../components/AgilanApp/AgilanApp";
 
 const AppScreens = (store, Provider) => {
   Navigation.registerComponentWithRedux(
     "Welcome",
     () => Welcome,
+    Provider,
+    store
+  );
+  Navigation.registerComponentWithRedux(
+    "AgilanApp",
+    () => AgilanApp,
     Provider,
     store
   );
